d1009e1e97880c56537ca91ab6cab4813aa09de1
CasianLW <ciorbacasian@gmail.com>
2023-03-03 10:44:45 +0100

    dev meal page structure

---
+++ src/components/MealPageComponent.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [],
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ title }}</h1>
+    <img :src="img" :alt="title" />
+  </div>
+  <div class="meal-info">
+    <p>{{ category }}</p>
+    <p>{{ area }}</p>
+  </div>
+  <div>
+    <p>{{ instructions }}</p>
+  </div>
+  <div>
+    <p v-for="(ingredient, index) in ingredients" :key="index">
+      {{ ingredient }}
+    </p>
+  </div>
+</template>

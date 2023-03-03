612fe4eac9a27d26c5ad72409a3dc0b1bcd61b8d
CasianLW <ciorbcasian@gmail.com>
2023-03-03 10:07:26 +0100

    dev nav bar with routes

---
+++ src/components/shared/NavComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  page: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="nav">
+    <a class="green">{{ page }}</a>
+    <router-link to="/">Home</router-link>
+    <router-link to="/random">Random</router-link>
+    <router-link to="/name">Search by Name</router-link>
+    <router-link to="/category">Search by Category</router-link>
+    <router-link to="/zone">Search by Zone</router-link>
+  </div>
+</template>

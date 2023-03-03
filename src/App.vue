0bd4529b9593d25f50a0b7e560aacd92a4603f9e
CasianLW <ciorbacasian@gmail.com>
2023-03-03 10:28:30 +0100

    router path get

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRoute } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import NavComponent from "./components/shared/NavComponent.vue";
+import { ref, watch } from "vue";
+
+const route = useRoute();
+const currentRoute = route.path;
+
+// Use a reactive statement to update the currentRoute variable whenever the route changes
+const routePath = ref(route.path);
+watch(route, () => {
+  routePath.value = route.path;
+});
 </script>
 
 <template>
@@ -17,7 +27,7 @@ import NavComponent from "./components/shared/NavComponent.vue";
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <NavComponent page="test" />
+      <NavComponent :page="routePath" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>

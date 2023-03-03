f6691455c8d06c726be83b213f0c37d63d91518b
CasianLW <ciorbcasian@gmail.com>
2023-03-03 11:39:59 +0100

    nav component fix nav

--- src/components/shared/NavComponent.vue
+++ src/components/shared/NavComponent.vue
@@ -8,12 +8,12 @@ defineProps({
 </script>
 
 <template>
-  <div class="nav">
+  <nav class="nav">
     <a class="green">{{ page }}</a> <br />
     <router-link to="/">Home</router-link>
     <router-link to="/random">Random</router-link>
     <router-link to="/name">Search by Name</router-link>
     <router-link to="/category">Search by Category</router-link>
     <router-link to="/zone">Search by Zone</router-link>
-  </div>
+  </nav>
 </template>

1a8070118ff966fc24ba17a3cdf7ef07f1f93f7e
CasianLW <ciorbcasian@gmail.com>
2023-03-03 10:28:23 +0100

    test br

--- src/components/shared/NavComponent.vue
+++ src/components/shared/NavComponent.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="nav">
-    <a class="green">{{ page }}</a>
+    <a class="green">{{ page }}</a> <br />
     <router-link to="/">Home</router-link>
     <router-link to="/random">Random</router-link>
     <router-link to="/name">Search by Name</router-link>

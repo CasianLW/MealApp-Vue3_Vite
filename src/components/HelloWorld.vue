498ef9883c12ff4ea0be7ca1b8144ed6e1711287
CasianLW <ciorbacasian@gmail.com>
2023-03-05 18:38:03 +0100

    better nav with icon and text logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,12 +9,23 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="https://static.vecteezy.com/system/resources/previews/007/742/645/original/cooking-book-cartoon-book-icon-with-orange-color-free-vector.jpg"
+      width="125"
+      height="125"
+    />
+    <p class="green">{{ msg }}</p>
   </div>
 </template>
 
 <style scoped>
-h1 {
+.greetings {
+  display: flex;
+  align-items: center;
+}
+p {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
@@ -24,13 +35,13 @@ h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
+.greetings p,
 .greetings h3 {
   text-align: center;
 }
 
 @media (min-width: 1024px) {
-  .greetings h1,
+  .greetings p,
   .greetings h3 {
     text-align: left;
   }

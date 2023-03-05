312cffaa2024bab52e77ac78ec1f7316f233a431
CasianLW <ciorbacasian@gmail.com>
2023-03-05 17:38:48 +0100

    imported meal card component

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -1,47 +1,14 @@
 <template>
-  <div class="meal-card" v-if="meal">
-    <div>
-      <h1>{{ meal.strMeal }}</h1>
-      <img :src="meal.strMealThumb" :alt="meal.strMeal" />
-    </div>
-    <div v-if="meal.strTags.length > 2">
-      <span
-        class="tag-item"
-        v-for="tag in meal.strTags.split(',')"
-        :key="tag"
-        >{{ tag }}</span
-      >
-    </div>
-    <div class="meal-tags">
-      <p><strong>Catégorie:</strong> {{ meal.strCategory }}</p>
-      <p><strong>Origine:</strong> {{ meal.strArea }}</p>
-    </div>
-    <div>
-      <h4>Details:</h4>
-      <p>{{ meal.strInstructions }}</p>
-    </div>
-    <div>
-      <h4>Ingrédients:</h4>
-      <ul>
-        <li v-for="ingredient in ingredients" :key="ingredient">
-          {{ ingredient }}
-        </li>
-      </ul>
-    </div>
-    <div v-if="meal.strYoutube.length > 5">
-      <a target="_blank" :href="meal.strYoutube">See how to on Youtube</a>
-    </div>
-    <div v-if="meal.strSource.length > 5">
-      <a target="_blank" :href="meal.strSource">->recipe credit</a>
-    </div>
-  </div>
+  <meal-card :mealId="meal.idMeal" v-if="meal" />
+
   <div v-else>
     <p>Loading meal details...</p>
   </div>
 </template>
 <script>
+import MealCard from "../components/shared/MealComponent.vue";
 export default {
-  name: "MealDetails",
+  name: "Meal",
   data() {
     return {
       meal: null,
@@ -50,6 +17,10 @@ export default {
   },
   mounted() {
     this.loadMeal();
+  },
+  components: {
+    name: "MealComponent",
+    MealCard,
   },
   methods: {
     async loadMeal() {

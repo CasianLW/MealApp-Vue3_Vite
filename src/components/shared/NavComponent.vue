03aa20b7dbc461b2f037812179029cc8724ecbdd
CasianLW <ciorbacasian@gmail.com>
2023-03-05 17:56:25 +0100

    index title, homepage and nav adjustments

--- src/components/shared/NavComponent.vue
+++ src/components/shared/NavComponent.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <nav class="nav">
-    <a class="green">{{ page }}</a> <br />
+    <!-- <a class="green">{{ page }}</a> <br /> -->
     <router-link to="/">Home</router-link>
     <router-link to="/random">Random</router-link>
     <router-link to="/name">Search by Name</router-link>
